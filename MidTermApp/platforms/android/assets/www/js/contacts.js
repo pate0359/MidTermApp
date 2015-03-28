7d2dfdeca2360254bc61324e74d118815d400829
Nignesh <NiGi@Nigneshs-MacBook-Pro.local>
2015-03-28 01:50:03 -0400

    Deleted previous "www"

--- MidTermApp/platforms/android/assets/www/js/contacts.js
+++ MidTermApp/platforms/android/assets/www/js/contacts.js
@@ -4,7 +4,7 @@ function getContacts() {
 	//	options.filter = "";
 	//	var fields = ["displayName", "name"];
 	options.multiple = true;
-	var fields = [navigator.contacts.fieldType.displayName, navigator.contacts.fieldType.name];
+	var fields = [navigator.contacts.fieldType.displayName, navigator.contacts.fieldType.name,navigator.contacts.fieldType.phoneNumber];
 	navigator.contacts.find(fields, onSuccess, onError, options);
 }
 
@@ -27,50 +27,7 @@ function onSuccess(contacts) {
 		}, 3000); //3secs
 	}
 
-	document.querySelector("#MyContacts").innerHTML = "";
-	for (var i = 0; i < contacts.length; i++) {
-
-		if (contacts[i].displayName) {
-			var li = document.createElement("li");
-			li.innerHTML = contacts[i].displayName;
-			li.setAttribute("data-ref", i);
-			document.querySelector('#MyContacts').appendChild(li);
-
-			addTapGestures(li);
-		}
-	}
-}
-
-// Hammer
-function addTapGestures(element) {
-	// Add Hammer double tap event
-	var mc = new Hammer.Manager(element);
-	// Tap recognizer with minimal 2 taps
-	mc.add(new Hammer.Tap({
-		event: 'doubletap',
-		taps: 2
-	}));
-	// Single tap recognizer
-	mc.add(new Hammer.Tap({
-		event: 'singletap'
-	}));
-	// we want to recognize this simulatenous, so a quadrupletap will be detected even while a tap has been recognized.
-	mc.get('doubletap').recognizeWith('singletap');
-	// we only want to trigger a tap, when we don't have detected a doubletap
-	mc.get('singletap').requireFailure('doubletap');
-
-	mc.on("singletap doubletap", function (ev) {
-
-		if (ev.type == "singletap") {
-			app.edit(ev.target);
-			//					alert(ev.type);
-			//					li.addEventListener("click", app.edit);
-
-		} else if (ev.type == "doubletap") {
-
-			alert(ev.target.innerHTML);
-		}
-	});
+	app.addContactListElement(contacts);
 }
 
 // onError: Failed to get the contacts
--- MidTermApp/platforms/android/assets/www/js/index.js
+++ MidTermApp/platforms/android/assets/www/js/index.js
@@ -13,16 +13,16 @@ var app = {
 	},
 	edit: function (ev) {
 
-			  //alert(ev.innerHTML);
-//		ev.stopPropagation();
+		//alert(ev.innerHTML);
+		//		ev.stopPropagation();
 		//    var item = ev.target.getAttribute("data-ref");
 		//    var itemVal = ev.target.innerHTML;
 		//    document.getElementById("list").value = item;
 		//	  alert(item);
 		//	  alert(itemVal);
 
-			  document.querySelector("[data-role=modal]").style.display="block";
-		    document.querySelector("[data-role=overlay]").style.display="block";
+		document.querySelector("[data-role=modal]").style.display = "block";
+		document.querySelector("[data-role=overlay]").style.display = "block";
 		document.querySelector(".contactName").innerHTML = ev.innerHTML;
 
 		/**************
@@ -37,6 +37,50 @@ var app = {
     ****************/
 
 		document.querySelector("[data-role=modal] h3").innerHTML = "Editing " + itemVal;
+	},
+	addContactListElement: function (contacts) {
+		document.querySelector("#MyContacts").innerHTML = "";
+		for (var i = 0; i < contacts.length; i++) {
+
+			if (contacts[i].displayName) {
+				var li = document.createElement("li");
+				li.innerHTML = contacts[i].displayName;
+				li.setAttribute("data-ref", i);
+				document.querySelector('#MyContacts').appendChild(li);
+
+				app.addHammerGestures(li);
+			}
+		}
+	},
+	addHammerGestures: function (element) {
+		// Add Hammer double tap event
+		var mc = new Hammer.Manager(element);
+		// Tap recognizer with minimal 2 taps
+		mc.add(new Hammer.Tap({
+			event: 'doubletap',
+			taps: 2
+		}));
+		// Single tap recognizer
+		mc.add(new Hammer.Tap({
+			event: 'singletap'
+		}));
+		// we want to recognize this simulatenous, so a quadrupletap will be detected even while a tap has been recognized.
+		mc.get('doubletap').recognizeWith('singletap');
+		// we only want to trigger a tap, when we don't have detected a doubletap
+		mc.get('singletap').requireFailure('doubletap');
+
+		mc.on("singletap doubletap", function (ev) {
+
+			if (ev.type == "singletap") {
+				app.edit(ev.target);
+				//					alert(ev.type);
+				//					li.addEventListener("click", app.edit);
+
+			} else if (ev.type == "doubletap") {
+
+				alert(ev.target.innerHTML);
+			}
+		});
 	}
 }
 
